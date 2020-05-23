2454a306a16a94ba106ab9a348ebbbd57c2475a6
rameswarmangaraj <rameswarmangaraj@gmail.com>
2020-05-24 00:31:22 +0530

    changed

--- com/accolite/FindCommUncommEle.java
+++ com/accolite/FindCommUncommEle.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class FindCommUncommEle {
 
     /**
-     * Two file contain some number in eachline f1={1,2,3,4,5,6,7,8,9,10}
+     * Two file contain some number in eachline  f1={1,2,3,4,5,6,7,8,9,10}
      * f2={2,6,7,9} Find common ele ? Find numbers in file1 that not in file2 ?
      */
     public static void main(String[] args) {
